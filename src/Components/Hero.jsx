d13d7f572a8b5a912db758af65cec8021ae78da6
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-02-15 16:35:35 +0530

    name changed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                     variants={container(0)}
                     initial="hidden"
                     animate="visible"
-                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Tanishk Agrawal</motion.h1>
+                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Tanishk Agrawalll</motion.h1>
                     <motion.span 
                     variants={container(0.5)}
                     initial="hidden"
